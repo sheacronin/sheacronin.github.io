30344a3c41ca65d4a768277e8392fb53ecf88a82
Shea Cronin <shealcronin@gmail.com>
2021-10-28 12:43:33 -0400

    Add initial Header and background colors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
-    return <div>Hello World</div>;
+    return (
+        <div>
+            <Header />
+            <main>Hello World</main>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import '../styles/Header.css';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Shea Cronin</h1>
+        </header>
+    );
+};
+
+export default Header;
