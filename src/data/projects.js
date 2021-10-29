ab28610dc1e6d164894fa9664679551cb161c544
Shea Cronin <shealcronin@gmail.com>
2021-10-29 19:09:34 -0400

    Add more projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,35 @@
+import battlebugImg from '../img/battlebug.png';
+import memoryCardImg from '../img/memory-card.png';
+import weatherImg from '../img/weather.png';
+import cvImg from '../img/cv.png';
+import todoImg from '../img/todo.png';
+
 const projects = [
-    { title: 'BattleBug', description: 'BattleBug description.', img: '#' },
-    { title: 'Memory Card', description: 'Memory Card description.', img: '#' },
+    {
+        title: 'BattleBug',
+        description: 'BattleBug description.',
+        image: battlebugImg,
+    },
+    {
+        title: 'Memory Card Game',
+        description: 'Memory Card description.',
+        image: memoryCardImg,
+    },
+    {
+        title: 'Weather App',
+        description: 'Weather App description.',
+        image: weatherImg,
+    },
+    {
+        title: 'CV Maker',
+        description: 'description.',
+        image: cvImg,
+    },
+    {
+        title: 'Todo List',
+        description: 'description.',
+        image: todoImg,
+    },
 ];
 
 export default projects;
