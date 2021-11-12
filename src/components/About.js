5ca41a386a7736a071f95935eef026be93163e42
Shea Cronin <shealcronin@gmail.com>
2021-11-12 08:54:56 -0500

    Add initial About section

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,33 @@
 import '../styles/About.css';
+import selfImg from '../img/self.jpg';
 
 const About = () => {
     return (
         <section id="about">
             <h2>About</h2>
-            <p>
-                Shea is a self-taught, Philadelphia-based programmer with a
-                strong drive to learn every day and to put that knowledge to
-                use. He has a background in film and video post-production,
-                collaborating with both technical and creative teams to deliver
-                polished projects. He is a summa cum laude Honors graduate of
-                Temple University who works hard to give his best to everything
-                he does.
-            </p>
+            <div id="about-info-container">
+                <img
+                    src={selfImg}
+                    alt="Shea with a background of fall leaves"
+                />
+                <div>
+                    <p>
+                        Shea is a self-taught, Philadelphia-based programmer
+                        with a strong drive to learn every day and to put that
+                        knowledge to use.
+                    </p>
+                    <p>
+                        He has a background in film and video post-production,
+                        collaborating with both technical and creative teams to
+                        deliver polished projects.
+                    </p>
+                    <p>
+                        He is a summa cum laude Honors graduate of Temple
+                        University who works hard to give his best to everything
+                        he does.
+                    </p>
+                </div>
+            </div>
         </section>
     );
 };
