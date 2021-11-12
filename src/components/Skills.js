d03b907c52f719b9ae5d7e4769c21e9d052071a0
Shea Cronin <shealcronin@gmail.com>
2021-11-12 08:21:09 -0500

    Add initial Skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,14 +1,45 @@
+import javascriptImg from '../img/icons/javascript.png';
+import reactImg from '../img/icons/react.png';
+import gitImg from '../img/icons/git.png';
+import htmlImg from '../img/icons/html.png';
+import cssImg from '../img/icons/css.png';
+import '../styles/Skills.css';
+
+const skillIcons = {
+    javascriptImg,
+    reactImg,
+    gitImg,
+    htmlImg,
+    cssImg,
+};
+
 const Skills = () => {
+    const skills = ['Javascript', 'React', 'Git', 'HTML', 'CSS'];
+
     return (
         <section id="skills">
             <h2>Skills</h2>
             <ul>
-                <li>JavaScript</li>
-                <li>React</li>
-                <li>Git</li>
-                <li>HTML & CSS</li>
+                {skills.map((skill) => (
+                    <SkillItem key={skill} skill={skill} />
+                ))}
             </ul>
         </section>
+    );
+};
+
+const SkillItem = (props) => {
+    const { skill } = props;
+
+    return (
+        <li className="skill-item">
+            <img
+                className="skill-icon"
+                src={skillIcons[`${skill.toLowerCase()}Img`]}
+                alt={skill}
+            />
+            {skill}
+        </li>
     );
 };
 
