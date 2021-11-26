90b4d2875c79cd7050fc4de3486a5537aa6628d4
Shea Cronin <shealcronin@gmail.com>
2021-11-26 13:19:59 -0500

    Add GitHub link to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,12 @@ const Header = () => {
     return (
         <header>
             <h1>shea cronin</h1>
+            <a href="https://github.com/sheacronin">
+                <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+                    alt="GitHub"
+                />
+            </a>
         </header>
     );
 };
